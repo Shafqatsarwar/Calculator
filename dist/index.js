e0fbe1a67a32f9a249ac7c7febdd4d7831de4993
Shafqatsarwar <139677807+Shafqatsarwar@users.noreply.github.com>
2023-10-20 01:18:27 +0100

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+inquirer.prompt([
+    { type: "number", name: "num1", message: "Enter your 1st Number", },
+    { type: "number", name: "num2", message: "Enter your 2nd Number", },
+    { type: "list", name: "operation", message: "Select Operation", choices: ["+", "-", "*", "/"], },
+])
+    .then((answers) => {
+    if (answers.operation == "+") {
+        const result = answers.num1 + answers.num2;
+        console.log("Sum", result);
+    }
+    else if (answers.operation == "-") {
+        const result = answers.num1 - answers.num2;
+        console.log("Subtract", result);
+    }
+    else if (answers.operation == "*") {
+        const result = answers.num1 * answers.num2;
+        console.log("Multiple", result);
+    }
+    else if (answers.operation == "/") {
+        const result = answers.num1 / answers.num2;
+        console.log("Division", result);
+    }
+})
+    .catch((error) => {
+    if (error.isTtyError) { }
+    else { }
+});
